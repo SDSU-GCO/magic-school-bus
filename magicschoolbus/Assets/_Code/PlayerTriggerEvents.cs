6b418417b8ce4366bd5078f488d6f50f90892719
Tyler <dreamingimlatios@gmail.com>
2019-03-17 14:46:15 -0500

    Setup Project (#1)
    ## I did the following:
    * Updated all Unity Package Manager packages relative to the project
    * Created a directory structure for assets
    * Modified Graphics Pipeline to use LWRP
    * Set Graphics to use Linear Color Space
    * Modified Player settings to reflect organization name, project name, and version
    * Modified Physics to use 60 fps timestep
    * Added PlayerTriggerEvents script
    
    ## I did not do the following
    * Configured input manager
    * Modified LWRP to use cel-shading
    * Configured Graphics Quality Tiers

---
+++ magicschoolbus/Assets/_Code/PlayerTriggerEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MSB
+{
+    public class PlayerTriggerEvents : MonoBehaviour
+    {
+        public UnityEvent TriggerEnter;
+        public UnityEvent TriggerExit;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                TriggerEnter.Invoke();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                TriggerEnter.Invoke();
+            }
+        }
+    }
+}
